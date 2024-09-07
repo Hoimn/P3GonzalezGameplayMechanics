9e548b446b123441f5d5a020aec7ca83795aa9a0
Hoimn <737753@student.dusd.net>
2024-09-07 14:44:30 -0700

    Finished the smash Powerup
    Finished the smash Powerup

--- GameplayMechanicsGonzalezP3/Assets/Scripts/PowerUp.cs
+++ GameplayMechanicsGonzalezP3/Assets/Scripts/PowerUp.cs
@@ -2,27 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum PowerUpType {  None, Pushback, Rockets, Smash}
+public enum PowerUpType {  None, Pushback, Rockets, Smash }
 
 public class PowerUp : MonoBehaviour
 {
-    public float hangTime;
-    public float smashSpeed; 
-    public float explosionForce; 
-    public float explosionRadius;
-    bool smashing = false;
-    float floorY;
-
     public PowerUpType powerUpType;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
 }
